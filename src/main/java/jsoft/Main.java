a6b1dd1854321aec3e7e70c81530199cbfb97695
handbob <handbob@icloud.com>
2024-03-02 21:36:32 +0100

    create new class for functions

--- src/main/java/jsoft/Main.java
+++ src/main/java/jsoft/Main.java
@@ -1,15 +1,6 @@
 package jsoft;
 
 public class Main {
-    public static int sum(int n) {
-        int s = 0;
-
-        for (int i = 1; i <= n; i++)
-            s += i;
-        
-        return s;
-    }
-
     public static void main(String[] args) {
         System.out.println("Hello from Java!");
     }
