91fa984dd720125fd124266ae134a8f3b35629c2
handbob <handbob@icloud.com>
2024-03-04 17:02:05 +0100

    change project

--- src/main/java/jsoft/JTools.java
+++ src/main/java/jsoft/JTools.java
@@ -3,8 +3,15 @@ package jsoft;
 public class JTools {
     public static int sum(int n) {
         var s = 0;
-        for (var i = 1; i <= n; i++)
+        for (var i = 0; i <= n; i++)
             s += i;
         return s;
+    }
+    public static int countWhitespaces(String str) {
+        var c = 0;
+        for (var i = 0; i < str.length(); i++)
+            if (Character.isWhitespace(str.charAt(i)))
+                c++;
+        return c;
     }
 }
--- src/main/java/jsoft/Main.java
+++ src/main/java/jsoft/Main.java
@@ -3,5 +3,7 @@ package jsoft;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Java!");
+        System.out.println(JTools.sum(10));
+        System.out.println(JTools.countWhitespaces("Programming is fun!"));
     }
 }
