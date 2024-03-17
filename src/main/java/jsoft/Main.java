8dadbabb12dfea2e7151db58ddc401b96cdc174f
handbob <handbob@icloud.com>
2024-03-17 18:05:30 +0100

    change project

--- src/main/java/jsoft/Main.java
+++ src/main/java/jsoft/Main.java
@@ -2,7 +2,7 @@ package jsoft;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from Java!");
+        System.out.println("Hello from Java " + System.getProperty("java.version"));
         System.out.println(JTools.sum(10));
         System.out.println(JTools.countWhitespaces("Programming is fun!"));
     }
